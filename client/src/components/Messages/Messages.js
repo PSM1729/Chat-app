b4908e4a1765bd8ab3a4f120903ec5320f9c7398
PSM1729 <sabitamishra1729@gmail.com>
2020-09-29 00:38:34 +0530

    added some files

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './Messages.css';
 
-import ScrollToButtom from 'react-scroll-to-bottom';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 import Message from './Message/Message';
 
-const Messages = ({ messages, name }) => (
-    <ScrollToButtom className="messages">
-        {messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
-    </ScrollToButtom>
+import './Messages.css';
 
+const Messages = ({ messages, name }) => (
+  <ScrollToBottom className="messages">
+    {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
+  </ScrollToBottom>
 );
 
 export default Messages;
