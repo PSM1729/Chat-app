198c495c8ea830762b0397d9c8a71dc1ab65c7f9
PSM1729 <sabitamishra1729@gmail.com>
2020-10-04 19:15:40 +0530

    Deleted some files

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import ScrollToBottom from 'react-scroll-to-bottom';
 
-import Message from './Message/Message';
-
 import './Messages.css';
 
-const Messages = ({ messages, name }) => (
-  <ScrollToBottom className="messages">
-    {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
+const Messages = ({ messages, name }) => {
+  <ScrollToBottom>
+    {messages.map((message, i)=><div key={i}><Message message={message} name={name}/></div>)}
   </ScrollToBottom>
-);
+}
 
 export default Messages;
